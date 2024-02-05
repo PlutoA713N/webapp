a790ad69038f8cbf21727497de86a18fc050a35b
PlutoA713N <mallachandu99@gmail.com>
2024-02-05 23:11:16 +0530

    Add 7-strike-rate-of-a-batsman-for-each-season.js

---
+++ src/server/7-stike-rate-of-a-batsman-for-each-season.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const deliveriesData = require("../data/deliveries.json");
+const matchesData = require("../data/matches.json");
+
+const strikeRateOfABatsmanForEachSeason = (batsman) => {
+  const outputFilePath =
+    "../public/output/strikeRateOfABatsmanForEachSeason.json";
+
+  const idsObject = {};
+  for (let i = 0; i < matchesData.length; i++) {
+    const curr = matchesData[i];
+    if (!idsObject[curr.season]) {
+      idsObject[curr.season] = [curr.id];
+    } else {
+      idsObject[curr.season].push(curr.id);
+    }
+  }
+
+  const object = {};
+
+  for (const key in idsObject) {
+    if (idsObject.hasOwnProperty(key)) {
+      object[key] = {};
+
+      for (let j = 0; j < deliveriesData.length; j++) {
+        const obj = deliveriesData[j];
+        if (idsObject[key].includes(obj.match_id)) {
+          object[key][obj.batsman] = object[key][obj.batsman] || {
+            runs: 0,
+            balls: 0,
+          };
+
+          object[key][obj.batsman].runs += parseInt(obj.batsman_runs);
+          object[key][obj.batsman].balls += 1;
+        }
+      }
+
+      for (const batsman in object[key]) {
+        if (object[key].hasOwnProperty(batsman)) {
+          const { runs, balls } = object[key][batsman];
+          object[key][batsman].strikeRate = +(
+            (runs / balls) * 100 || 0
+          ).toFixed(2);
+        }
+      }
+    }
+  }
+
+  const writeStream = fs.createWriteStream(outputFilePath);
+  writeStream.write(JSON.stringify(object, null, 2));
+  writeStream.end();
+
+  writeStream.on("finish", () => {
+    console.log("Output file created successfully");
+  });
+
+  writeStream.on("error", (err) => {
+    console.log("Failed to write data:", err);
+  });
+};
+
+strikeRateOfABatsmanForEachSeason("V Kohli");
