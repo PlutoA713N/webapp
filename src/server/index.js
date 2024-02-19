0dfdd9473962051a47c3f75b744f92953deb74b4
PlutoA713N <mallachandu99@gmail.com>
2024-02-19 17:05:08 +0530

    Add params to strikeRate Function

--- src/server/index.js
+++ src/server/index.js
@@ -1,37 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const path = require('path')
-const PORT = 3000
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs'); 
+const path = require('path');
 
-const fs = require("fs");
 const matches = require("../data/matches.json");
 const deliveries = require("../data/deliveries.json");
+const cricketAnalyzer = require('./ipl.js');
 
-const cricketAnalyzer = require('./ipl.js'); 
+const app = express();
+const PORT = 3000;
 
-cricketAnalyzer.matchesPerYear(matches);
-cricketAnalyzer.matchesWonPerTeamPerYear(matches);
-cricketAnalyzer.extraRunsConceededPerTeamInTheYear(matches, 2016);
-cricketAnalyzer.top10EconomicalBowlersInTheYear(matches, 2015);
-cricketAnalyzer.timesEachTeamWontheTossAndWonTheMatch(matches);
-cricketAnalyzer.playerOfTheMatchAwardsForEachSeason(matches);
-cricketAnalyzer.strikeRateOfABatsmanForEachSeason("V Kohli");
-cricketAnalyzer.highestTimesPlayerDismissedByAnotherPlayer(matches);
-cricketAnalyzer.bowlerWithBestEconomy(matches);
+async function analyzeCricketData() {
+    try {
+        await cricketAnalyzer.matchesPerYear(matches);
+        await cricketAnalyzer.matchesWonPerTeamPerYear(matches);
+        await cricketAnalyzer.extraRunsConceededPerTeamInTheYear(matches, 2016);
+        await cricketAnalyzer.top10EconomicalBowlersInTheYear(matches, 2015);
+        await cricketAnalyzer.timesEachTeamWontheTossAndWonTheMatch(matches);
+        await cricketAnalyzer.playerOfTheMatchAwardsForEachSeason(matches);
+        await cricketAnalyzer.strikeRateOfABatsmanForEachSeason("V Kohli", deliveries, matches);
+        await cricketAnalyzer.highestTimesPlayerDismissedByAnotherPlayer(matches);
+        await cricketAnalyzer.bowlerWithBestEconomy(matches);
+    } catch (error) {
+        console.error('An error occurred during cricket data analysis:', error.message);
+    }
+}
 
-
-app.use(cors())
+app.use(cors());
 
 const outputDir = path.join(__dirname, '../public/output/');
 app.use(express.static(outputDir));
-
 
 app.get('/', (req, res) => {
     res.send('Output files are being served!');
 });
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, async () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    await analyzeCricketData();
+});
