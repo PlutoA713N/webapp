113af4ba7ffa5340571219f58f05a2028a8301f4
PlutoA713N <mallachandu99@gmail.com>
2024-02-19 16:32:15 +0530

    Start a static server & host json files

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,23 @@ const app = express()
 const path = require('path')
 const PORT = 3000
 
+const fs = require("fs");
+const matches = require("../data/matches.json");
+const deliveries = require("../data/deliveries.json");
+
+const cricketAnalyzer = require('./ipl.js'); 
+
+cricketAnalyzer.matchesPerYear(matches);
+cricketAnalyzer.matchesWonPerTeamPerYear(matches);
+cricketAnalyzer.extraRunsConceededPerTeamInTheYear(matches, 2016);
+cricketAnalyzer.top10EconomicalBowlersInTheYear(matches, 2015);
+cricketAnalyzer.timesEachTeamWontheTossAndWonTheMatch(matches);
+cricketAnalyzer.playerOfTheMatchAwardsForEachSeason(matches);
+cricketAnalyzer.strikeRateOfABatsmanForEachSeason("V Kohli");
+cricketAnalyzer.highestTimesPlayerDismissedByAnotherPlayer(matches);
+cricketAnalyzer.bowlerWithBestEconomy(matches);
+
+
 app.use(cors())
 
 const outputDir = path.join(__dirname, '../public/output/');
