023c5a1148d6c042e00f3ea2d3c96601799e0885
PlutoA713N <mallachandu99@gmail.com>
2024-02-19 06:44:35 +0530

    Create a static server

--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,13 @@ const PORT = 3000
 app.use(cors())
 
 const outputDir = path.join(__dirname, '../public/output/');
-console.log('Output directory:', outputDir);
 app.use(express.static(outputDir));
+
+
+app.get('/', (req, res) => {
+    res.send('Output files are being served!');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
